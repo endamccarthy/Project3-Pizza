d99f5dc87218b83eaeadc0ca3eabb1b837b26cff
Enda McCarthy <enda.mccarthy@outlook.com>
2019-07-22 14:59:16 +0100

    venv added

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/endamccarthy/Library/Mobile Documents/com~apple~CloudDocs/00 Enda/01 Career/00 Education/02 Post Grad/07 CS50 Web/Project3-Pizza/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
